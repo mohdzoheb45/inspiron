2ca9de63c28f5337f5d50c486fc236adcfcc054f
mohdzoheb45 <86587804+mohdzoheb45@users.noreply.github.com>
2021-07-10 18:38:44 +0530

    program num1

---
+++ additionof2nubers.java
@@ -0,0 +1,14 @@
+package hit.day25;
+
+
+public class additionof2nubers {  
+	public static void main (String[]args)
+	{
+		System.out.println ("Sum of two numbers");
+		int first=100;
+		int second =200;
+				System.out.println(first + "  " + second);
+		int sum =first+second;
+				System.out.print("The sum is +" +sum);
+      }
+}
