de650493938883bab921cc58ca31f1f166808779
mohdzoheb45 <86587804+mohdzoheb45@users.noreply.github.com>
2021-07-10 22:43:00 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,14 @@
+package hit.day25;
+
+public class typecasting {
+	public static void main(String[] args) {
+		byte b=10;
+		int i=b;
+		
+     byte d=(byte)i;
+     byte x=10;
+     byte y=20;
+     byte sum=(byte) (x*y);
+	}
+
+}
