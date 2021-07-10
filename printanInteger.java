408e43717a814ce365c53847e6e959191576097d
mohdzoheb45 <86587804+mohdzoheb45@users.noreply.github.com>
2021-07-10 19:08:42 +0530

    Add files via upload
    program num2

---
+++ printanInteger.java
@@ -0,0 +1,19 @@
+package hit.day25;
+import java.util.Scanner;
+
+public class printanInteger {
+	 public static void main(String[] args) {
+		 
+	 
+	 { 
+	Scanner reader=new Scanner(System.in);
+	System.out.print("Enter a nuumber: ");
+	
+	int number=reader.nextInt();
+	System.out.println("You entered:  "  +  number);
+			
+	
+
+}
+}
+	 }
