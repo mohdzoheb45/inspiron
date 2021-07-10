0de9f81ebd97939b2e1942ef5a14cc90852cbfb1
mohdzoheb45 <86587804+mohdzoheb45@users.noreply.github.com>
2021-07-10 19:23:06 +0530

    multiplication

---
+++ mul2num.java
@@ -0,0 +1,22 @@
+package hit.day25;
+import java.util.Scanner;
+
+
+public class mul2num {
+	 public static void main(String[] args)
+	 { 
+		 Scanner Scan=new Scanner(System.in);
+		 System.out.println("Enter the value for i....: ");
+		 int i=Scan.nextInt();
+		 System.out.println("Enter values for j....");
+		 int j=Scan.nextInt();
+	int  mul=i*j;
+		 System.out.println("The multiplication of i and j is :" +mul);
+		 
+		 
+		 
+	 }
+}
+	
+
+
